4d39d705b388d233f3a9c23debde3917be832626
Владислав <zeyger@icloud.com>
2018-08-05 19:20:03 +0300

    Day 4 Task 2 Geometric Distribution I done

---
+++ 10 days of statistics/Day 4/Geometric Distribution I.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def geometric_distribution(fail_prob, fail_index):
+    assert 1 >= fail_prob >= 0
+    assert fail_index > 0
+    succ_prob = 1 - fail_prob
+    succ_prob_mult = pow(succ_prob, fail_index - 1)
+    result = succ_prob_mult * fail_prob
+    return result
+
+
+def main():
+    line = sys.stdin.readline()
+    num, den = list(map(int, line.split(' ')))
+    fail_index = int(input())
+    result = geometric_distribution(num / den, fail_index)
+
+    print("{0:.3f}".format(result))
+
+
+if __name__ == '__main__':
+    main()
